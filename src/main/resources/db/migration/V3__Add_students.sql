142d231aba128905852090575d55d72251fe585f
DanilWH <danil-lomakin-02@mail.ru>
2021-04-04 18:33:45 +0500

    Database migration - Add students.

---
+++ src/main/resources/db/migration/V3__Add_students.sql
@@ -0,0 +1,208 @@
+insert into users (id, first_name, last_name, login, password) values (
+	1,
+	'Никита',
+	'Борцов',
+	'БорцовНикита',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (1, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	2,
+	'Никита',
+	'Бояршинов',
+	'БояршиновНикита',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (2, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	3,
+	'Тимофей',
+	'Власов',
+	'ВласовТимофей',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (3, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	4,
+	'Даниил',
+	'Гайфуллин',
+	'ГайфуллинДаниил',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (4, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	5,
+	'Кирилл',
+	'Глухих',
+	'ГлухихКирилл',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (5, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	6,
+	'Диана',
+	'Горбачёва',
+	'ГорбачёваДиана',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (6, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	7,
+	'Николай',
+	'Дегонский',
+	'ДегонскийНиколай',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (7, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	8,
+	'Тимур',
+	'Зайдулин',
+	'ЗайдулинТимур',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (8, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	9,
+	'Игорь',
+	'Копытов',
+	'КопытовИгорь',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (9, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	10,
+	'Денис',
+	'Кулюкин',
+	'КулюкинДенис',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (10, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	11,
+	'Данил',
+	'Ломакин',
+	'ЛомакинДанил',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (11, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	12,
+	'Даниил',
+	'Маланчуков',
+	'МаланчуковДаниил',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (12, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	13,
+	'Игроь',
+	'Малофеев',
+	'МалофеевИгроь',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (13, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	14,
+	'Оксана',
+	'Муллаярова',
+	'МуллаяроваОксана',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (14, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	15,
+	'Александр',
+	'Орлов',
+	'ОрловАлександр',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (15, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	16,
+	'Олег',
+	'Пигалов',
+	'ПигаловОлег',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (16, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	17,
+	'Данила',
+	'Ролдугин',
+	'РолдугинДанила',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (17, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	18,
+	'Августина',
+	'Смольникова',
+	'СмольниковаАвгустина',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (18, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	19,
+	'Михаил',
+	'Собакин',
+	'СобакинМихаил',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (19, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	20,
+	'Вадим',
+	'Талыпов',
+	'ТалыповВадим',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (20, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	21,
+	'Михаил',
+	'Уклов',
+	'УкловМихаил',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (21, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	22,
+	'Никита',
+	'Четвериков',
+	'ЧетвериковНикита',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (22, 'STUDENT');
+
+insert into users (id, first_name, last_name, login, password) values (
+	23,
+	'Виктор',
+	'Ширягин',
+	'ШирягинВиктор',
+	'$2y$12$rR7yaX6pbAN/glad6bBypuShvyAq3SS9RMphujBs99Yq8f2hK5AaW'
+);
+insert into user_roles (user_id, user_roles) values (23, 'STUDENT');
+
+update hibernate_sequence set next_val = 24;
