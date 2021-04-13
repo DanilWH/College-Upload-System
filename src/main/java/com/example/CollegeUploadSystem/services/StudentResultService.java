e5f4b3676942513ce294690fed351a7542ee0aa2
DanilWH <danil-lomakin-02@mail.ru>
2021-04-14 00:24:10 +0500

    [ADDED] the ability to download students results as files.

--- src/main/java/com/example/CollegeUploadSystem/services/StudentResultService.java
+++ src/main/java/com/example/CollegeUploadSystem/services/StudentResultService.java
@@ -53,7 +53,7 @@ public class StudentResultService {
             // uploadPath is the root directory where all the upload are stored.
             // filepath is the directory specified by the group name and the task name.
             // filename is the unique file name.
-            String filepath = String.format("/%s/%s/", group.getName(), task.getName());
+            String filepath = String.format("%s/%s/", group.getName(), task.getName());
 
             // create a new directory if doesn't exist.
             File fileObj = new File(this.uploadPath + filepath);
