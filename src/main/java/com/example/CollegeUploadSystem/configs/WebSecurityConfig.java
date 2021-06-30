5796caae95af0b85e657071fcd88c35ef7dfdbe2
DanilWH <danil-lomakin-02@mail.ru>
2021-06-30 18:08:45 +0300

    [FIXED] The problem with not loading icons for guests.

--- src/main/java/com/example/CollegeUploadSystem/configs/WebSecurityConfig.java
+++ src/main/java/com/example/CollegeUploadSystem/configs/WebSecurityConfig.java
@@ -37,6 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                             "/groups",
                             "/group/*/students",
                             "/css/**",
+                            "/img/**",
                             "/file/**"
                     ).permitAll()
                     .anyRequest().authenticated()
