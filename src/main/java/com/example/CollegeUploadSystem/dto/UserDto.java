af0fd7f4a983dba5e5968f4a83abd5e4b8fce8dc
DanilWH <danil-lomakin-02@mail.ru>
2022-02-11 13:25:57 +0300

    [ADDED] The 'roles' field in the UserDto.java

--- src/main/java/com/example/CollegeUploadSystem/dto/UserDto.java
+++ src/main/java/com/example/CollegeUploadSystem/dto/UserDto.java
@@ -1,10 +1,12 @@
 package com.example.CollegeUploadSystem.dto;
 
 import com.example.CollegeUploadSystem.models.User;
+import com.example.CollegeUploadSystem.models.UserRoles;
 import com.example.CollegeUploadSystem.models.Views;
 import com.fasterxml.jackson.annotation.JsonView;
 
 import java.io.Serializable;
+import java.util.Set;
 
 public class UserDto implements Serializable {
     @JsonView(Views.Id.class)
@@ -21,6 +23,9 @@ public class UserDto implements Serializable {
     private String login;
 
     @JsonView(Views.FullProfile.class)
+    private Set<UserRoles> roles;
+
+    @JsonView(Views.FullProfile.class)
     private Long groupId;
 
     public UserDto(User user) {
@@ -29,6 +34,7 @@ public class UserDto implements Serializable {
         this.lastName = user.getLastName();
         this.fatherName = user.getFatherName();
         this.login = user.getLogin();
+        this.roles = user.getUserRoles();
         this.groupId = (user.getGroup() == null)? null : user.getGroup().getId();
     }
 
@@ -54,6 +60,11 @@ public class UserDto implements Serializable {
 
     public UserDto setLogin(String login) {
         this.login = login;
+        return this;
+    }
+
+    public UserDto setRoles(Set<UserRoles> roles) {
+        this.roles = roles;
         return this;
     }
 
