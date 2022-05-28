21ea68ad4d8568517976e19b8e692f173ebdc28a
DanilWH <danil-lomakin-02@mail.ru>
2022-05-28 18:33:58 +0500

    [FIXED] GroupMapper and TaskMapper do not map excessive fields now.

--- src/main/java/com/example/CollegeUploadSystem/mappers/GroupMapper.java
+++ src/main/java/com/example/CollegeUploadSystem/mappers/GroupMapper.java
@@ -2,13 +2,15 @@ package com.example.CollegeUploadSystem.mappers;
 
 import com.example.CollegeUploadSystem.dto.GroupDto;
 import com.example.CollegeUploadSystem.models.Group;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface GroupMapper {
 
-    @Mapping(target = "id", ignore = true)
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "name")
     Group toEntity(GroupDto groupDto);
 
     GroupDto toDto(Group group);
--- src/main/java/com/example/CollegeUploadSystem/mappers/TaskMapper.java
+++ src/main/java/com/example/CollegeUploadSystem/mappers/TaskMapper.java
@@ -2,13 +2,15 @@ package com.example.CollegeUploadSystem.mappers;
 
 import com.example.CollegeUploadSystem.dto.TaskDto;
 import com.example.CollegeUploadSystem.models.Task;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring", uses = { GroupMapper.class })
 public interface TaskMapper {
 
-    @Mapping(target = "id", ignore = true)
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "name")
     Task toEntity(TaskDto taskDto);
 
     @Mapping(target = "groupDto", source = "task.group")
